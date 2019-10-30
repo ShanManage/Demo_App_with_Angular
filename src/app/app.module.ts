7cf3f562c880bb91353fd257c50bb00a138672eb
ShanManage <shan.96rt@gmail.com>
2019-10-30 23:41:15 +0530

    Generate components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,43 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './app-material.module';
 import { RoomsComponent } from './accomodation/rooms/rooms.component';
 import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-beverage/bars/bars.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WelnessAndSpaComponent } from './lifestyle/welness-and-spa/welness-and-spa.component';
+import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
+import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
+import { ExcursionsComponent } from './lifestyle/excursions/excursions.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { ThaiWeddingsComponent } from './weddings/thai-weddings/thai-weddings.component';
+import { WesternsWeddingsComponent } from './weddings/westerns-weddings/westerns-weddings.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { OffersComponent } from './offers/offers.component';
+import { AuthComponent } from './auth/auth.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccomodationComponent,
     RoomsComponent,
-    FoodBeverageComponent
+    FoodBeverageComponent,
+    RestaurantComponent,
+    BarsComponent,
+    LifestyleComponent,
+    WelnessAndSpaComponent,
+    ResortActivitiesComponent,
+    ExperiencesComponent,
+    ExcursionsComponent,
+    WeddingsComponent,
+    ThaiWeddingsComponent,
+    WesternsWeddingsComponent,
+    CelebrationsComponent,
+    OffersComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
