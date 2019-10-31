e51c0cbe73b8cf811a323a31bfcc420ba257933f
ShanManage <shan.96rt@gmail.com>
2019-10-31 23:21:01 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { WeddingsComponent } from './weddings/weddings.component';
+import { OffersComponent } from './offers/offers.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'accomodation' , component : AccomodationComponent },
+  { path : 'celebrations' , component : CelebrationsComponent },
+  { path : 'food-beverage' , component : FoodBeverageComponent },
+  { path : 'lifestyle' , component : LifestyleComponent },
+  { path : 'offers' , component : OffersComponent },
+  { path : 'weddings' , component : WeddingsComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
