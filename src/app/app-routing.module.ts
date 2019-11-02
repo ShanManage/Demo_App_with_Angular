398d6838baf30418047fbd410ae59e8d29d70c79
ShanManage <shan.96rt@gmail.com>
2019-11-02 22:26:45 +0530

    Complete app routing modules ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
-import { WeddingsComponent } from './weddings/weddings.component';
-import { OffersComponent } from './offers/offers.component';
-import { LifestyleComponent } from './lifestyle/lifestyle.component';
-import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
-import { CelebrationsComponent } from './celebrations/celebrations.component';
-import { AccomodationComponent } from './accomodation/accomodation.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OffersComponent } from './offers/offers.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
 
 const routes: Routes = [
+  { path : 'food-beverage' , component : FoodBeverageComponent },
   { path : 'accomodation' , component : AccomodationComponent },
   { path : 'celebrations' , component : CelebrationsComponent },
-  { path : 'food-beverage' , component : FoodBeverageComponent },
   { path : 'lifestyle' , component : LifestyleComponent },
+  { path : 'weddings' , component : WeddingsComponent },
   { path : 'offers' , component : OffersComponent },
-  { path : 'weddings' , component : WeddingsComponent }
-  { path : 'signin' , component : SigninComponent }
+  { path : 'signin' , component : SigninComponent },
   { path : 'signup' , component : SignupComponent }
 
 ];
